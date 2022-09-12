10d62075c7f013981cd0613350c5246a773cc46c
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-12 19:56:53 +0300

    modify

--- Hello World/hello_world.py
+++ Hello World/hello_world.py
@@ -3,22 +3,18 @@ print("Hello World")
 name = "Rabab"
 print ("Hello",name)
 
-name = "Rabab"
 print ("Hello"+" "+name)
 
 num = 5
 print ("Hello",num)
-
-#num = 5                             
+                        
 #print ("Hello"+" "+num)            # I should use str() to solve the problem
 
-num = 5
 print ("Hello"+" "+str(num))
 
-food_one="apple"
-food_two="egg"
-print("I love to eat {} and {}".format(food_one,food_two))
 
-food_one="apple"
-food_two="egg"
-print(f"I love to eat {food_one} and {food_two}")
+fave_food1="apple"
+fave_food2="egg"
+print("I love to eat {} and {}".format(fave_food1,fave_food2))
+
+print(f"I love to eat {fave_food1} and {fave_food2}")
