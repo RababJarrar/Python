71d34f226867e3fcacf16a5eac1f4daec6398b36
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-13 13:16:52 +0300

    modify

--- Chaining Methods/assignment.py
+++ Chaining Methods/assignment.py
@@ -6,30 +6,19 @@ class User:
 
     def make_deposit(self,amount):     #method
         self.account_balance+= amount
-
+        return self
     def make_withdrawl(self,amount):     #method
         self.account_balance-= amount
+        return self
     def display_user_balance(self):
         print("User:",self.name,"balance:",self.account_balance)
-
+       
 rabab=User("RABAB")
 hadeel=User("HADEEL")
 maryam=User("MARYAM")
 
-rabab.make_deposit(100)
-rabab.make_deposit(200)
-rabab.make_deposit(300)
-rabab.make_withdrawl(400)
-rabab.display_user_balance()
+rabab.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawl(400).display_user_balance()
 
-hadeel.make_deposit(1000)
-hadeel.make_deposit(500)
-hadeel.make_withdrawl(500)
-hadeel.make_withdrawl(500)
-hadeel.display_user_balance()
+hadeel.make_deposit(1000).make_deposit(500).make_withdrawl(500).make_withdrawl(500).display_user_balance()
 
-maryam.make_deposit(1000)
-maryam.make_withdrawl(200)
-maryam.make_withdrawl(200)
-maryam.make_withdrawl(200)
-maryam.display_user_balance()
+maryam.make_deposit(1000).make_withdrawl(200).make_withdrawl(200).make_withdrawl(200).display_user_balance()
