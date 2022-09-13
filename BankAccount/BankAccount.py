c60b464c0835c4a0cafba1999f4ec1685241e519
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-13 14:46:16 +0300

    initial

---
+++ BankAccount/BankAccount.py
@@ -0,0 +1,33 @@
+
+class BankAccount:
+    def __init__(self, int_rate=.02, balance=0):            #default value .02 , 0
+        self.int_rate=int_rate                         
+        self.balance=balance
+
+    def deposit(self, amount):
+        self.balance+=amount
+        return self
+        
+    def withdraw(self, amount):
+        if (self.balance >= amount):
+            self.balance-=amount
+            return self
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance-=amount
+            return self
+
+    def yield_interest(self):
+        if(self.balance>0):
+            self.balance=(1+self.int_rate)*self.balance
+            return self
+    
+    def display_account_info(self):
+        print("Balance: $",self.balance)
+
+rabab=BankAccount(.02,200)
+# rabab.display_account_info()             
+rabab.deposit(100).deposit(100).deposit(50).withdraw(50).yield_interest().display_account_info()
+
+maryam=BankAccount(0.05,1000)           
+maryam.deposit(1000).deposit(1000).withdraw(500).withdraw(500).withdraw(500).withdraw(500).yield_interest().display_account_info()
