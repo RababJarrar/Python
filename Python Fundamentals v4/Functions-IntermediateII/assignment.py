32ad6137a645cea5d32cb5b5dbc79c8abd479476
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-14 15:07:15 +0300

    modify

--- Python Fundamentals v4/Functions-IntermediateII/assignment.py
+++ Python Fundamentals v4/Functions-IntermediateII/assignment.py
@@ -1,49 +1,52 @@
-# x = [ [5,2,3], [10,8,9] ] 
-# x[1][0]=15
-# print(x)
+#1
+x = [ [5,2,3], [10,8,9] ] 
+x[1][0]=15
+print(x)
 
-# students = [
-#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#      {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# students[0]['last_name'] ='Bryant'
-# print (students)
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+students[0]['last_name'] ='Bryant'
+print (students)
 
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# sports_directory['soccer'][0]='Andres'
-# print(sports_directory)
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+sports_directory['soccer'][0]='Andres'
+print(sports_directory)
 
-# z = [ {'x': 10, 'y': 20} ]
-# z[0]['y']=30
-# print(z)
-
-# students = [
-#          {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#          {'first_name' : 'John', 'last_name' : 'Rosales'},
-#          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#          {'first_name' : 'KB', 'last_name' : 'Tonel'}
-#     ]
-# def iterateDictionary(students):
-#     for i in range(0,len(students),1):
-#         print('first_name-',students[i]['first_name'],',last_name-',students[i]['last_name'])
-# iterateDictionary(students)
+z = [ {'x': 10, 'y': 20} ]
+z[0]['y']=30
+print(z)
 
 
-# #3.Get Values From a List of Dictionaries
-# students = [
-#          {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#          {'first_name' : 'John', 'last_name' : 'Rosales'},
-#          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#          {'first_name' : 'KB', 'last_name' : 'Tonel'}
-#     ]
-# def iterateDictionary2(key_name, some_list):
-#     for i in range(0,len(some_list),1):
-#         print(some_list[i][key_name])
-# iterateDictionary2('first_name', students)
-# iterateDictionary2('last_name', students)
+#2
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(students):
+    for i in range(0,len(students),1):
+        print('first_name-',students[i]['first_name'],',last_name-',students[i]['last_name'])
+iterateDictionary(students)
+
+
+#3.Get Values From a List of Dictionaries
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary2(key_name, some_list):
+    for i in range(0,len(some_list),1):
+        print(some_list[i][key_name])
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
 
 
 # 4.Iterate Through a Dictionary with List Values
