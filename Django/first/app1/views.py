29da4aa816948d5d98ef59f8643afc6e91204b35
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-20 15:05:17 +0300

    modify

---
+++ Django/first/app1/views.py
@@ -0,0 +1,31 @@
+
+from django.shortcuts import render,HttpResponse,redirect
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def root_method(request):
+    return redirect('blogs/')
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect('/')
+
+
+def show(request,number):
+    context = {
+        "num": number,
+    }
+    return render(request,'index.html',context)   
+    
+def edit(request,number):
+    context = {
+        "num": number,
+    }
+    return render(request,'index2.html',context)  
+
+
+def destroy(request,number):
+    return redirect('/')
---
+++ Django/seconed/app2/views.py
@@ -0,0 +1,12 @@
+
+def one_method(request):
+    pass
+    
+def another_method(request, my_val):
+    pass
+    
+def yet_another(request, name):
+    pass
+    
+def one_more(request, id, color):
+    pass
