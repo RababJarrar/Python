a9c8372cd1dcf291f90e95ff4040916499266f0c
RababJarrar <rababjarrar1997@yahoo.com>
2022-10-01 22:25:32 +0300

    modify

--- Django/Books_Authors/app_one/urls.py
+++ Django/Books_Authors/app_one/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('', views.index),
     path('add_book', views.addbook),
     path('books/<int:id>', views.bookdesc),
-    path('add', views.selected_author),
+    path('add/<int:id>', views.selected_author),
 
-
+    path('authors', views.index2),
+    path('add_author', views.addauthor),
+    path('authors/<int:id>', views.authordesc),
+    path('add2/<int:id>', views.selected_book),
 ]
--- Django/Books_Authors/app_one/views.py
+++ Django/Books_Authors/app_one/views.py
@@ -21,13 +21,57 @@ def bookdesc(request,id):
        "the_book" : Book.objects.get(id=id),
        "the_authors" : (Book.objects.get(id=id)).authors.all(),
        "all_the_authors" : Author.objects.all(),
-    
     }
     # print(context['the_authors'])
     return render(request,'display_book.html',context)
 
-def selected_author(request):
-    this_book=Book.objects.get(request.POST['mar'])
-    id_for_author = request.POST['select1']    
-    this_book.authors.add(id_for_author)
-    return redirect('books/<int:id>')
+def selected_author(request,id):
+    
+    this_author=Author.objects.get(id=request.POST['select_add'])
+    print(this_author)
+    this_book=Book.objects.get(id=id)  
+    print(this_book)
+    this_book.authors.add(this_author)
+
+    context={
+       "the_book" : Book.objects.get(id=id),
+       "the_authors" : (Book.objects.get(id=id)).authors.all(),
+       "all_the_authors" : Author.objects.all(),
+    }
+    return render(request,'display_book.html',context)
+
+#########
+def index2(request):
+    context={
+        "all_the_authors" : Author.objects.all(),
+    }
+    return render(request,'index2.html',context)
+
+def addauthor(request):
+    FNAME= request.POST['fname']
+    LNAME= request.POST['lname']
+    NOTE= request.POST['note']
+    Author.objects.create(first_name=FNAME,last_name=LNAME,notes=NOTE)
+    return redirect('/authors')
+
+def authordesc(request,id):
+    context={
+       "the_author" : Author.objects.get(id=id),
+       "the_books" : (Author.objects.get(id=id)).books.all(),
+       "all_the_books" : Book.objects.all(),
+    }
+    print(context['the_author'])
+    return render(request,'display_author.html',context)
+
+def selected_book(request,id):
+    
+    this_book=Book.objects.get(id=request.POST['select_add2'])
+    this_author=Author.objects.get(id=id)  
+    this_author.books.add(this_book)
+    context={
+       "the_author" : Book.objects.get(id=id),
+       "the_books" : (Author.objects.get(id=id)).books.all(),
+       "all_the_books" : Book.objects.all(),
+    }
+
+    return render(request,'display_author.html',context)
